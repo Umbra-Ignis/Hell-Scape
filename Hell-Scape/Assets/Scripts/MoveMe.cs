d9658701fd2ed5b5b0d59df7a13cf7252baedd19
Umbra-Ignis <devon.j.sanders@gmail.com>
2017-05-22 10:49:47 +1000

    First level greyboxed with navmeshed and enemy position

--- Hell-Scape/Assets/Scripts/MoveMe.cs
+++ Hell-Scape/Assets/Scripts/MoveMe.cs
@@ -16,48 +16,48 @@ public class MoveMe : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 //------------------------------WASD Movement-----------------------------------
-		//if (Input.GetKey (KeyCode.W)) {
-		//	transform.position = transform.position + new Vector3 (0, 0, 0.1f);
-		//}
-		//if (Input.GetKey (KeyCode.A)) {
-		//	transform.position = transform.position + new Vector3 (-0.1f, 0, 0);	
-		//}
-		//if (Input.GetKey (KeyCode.S)) {
-		//	transform.position = transform.position + new Vector3 (0, 0, -0.1f);	
-		//}
-		//if (Input.GetKey (KeyCode.D)) {
-		//	transform.position = transform.position + new Vector3 (0.1f, 0, 0);	
-		//}
-		//if (Input.GetKey (KeyCode.LeftControl)) {
-		//	transform.position = transform.position + new Vector3 (0.1f, 0, 0);	
-		//}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position = transform.position + new Vector3 (0, 0, 0.1f);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position = transform.position + new Vector3 (-0.1f, 0, 0);	
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position = transform.position + new Vector3 (0, 0, -0.1f);	
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position = transform.position + new Vector3 (0.1f, 0, 0);	
+		}
+		if (Input.GetKey (KeyCode.LeftControl)) {
+			transform.position = transform.position + new Vector3 (0.1f, 0, 0);	
+		}
 		//if (Input.GetKey (KeyCode.LeftArrow)) {
 		//	transform.Rotate (new Vector3 (0, 1, 0));	
 		//}
 //--------------------------Thrusting controller------------------------------
 		if (Input.GetKey (KeyCode.Space)) {
 			gameObject.GetComponent<Rigidbody> ().AddForce (transform.up * 20);
-		}
-		if (Input.GetKey (KeyCode.W)) {
-			gameObject.GetComponent<Rigidbody> ().AddForce (transform.forward * 15);
-		}
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Rotate (new Vector3 (0, 1, 0));
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Rotate (new Vector3 (0, -1, 0));
-		}
-		if (Input.GetKey (KeyCode.UpArrow)) {
-			transform.Rotate (new Vector3 (1, 0, 0));
-		}
-		if (Input.GetKey (KeyCode.DownArrow)) {
-			transform.Rotate (new Vector3 (-1, 0, 0));
-		}
-		if (Input.GetKey (KeyCode.Q)) {
-			transform.Rotate (new Vector3 (0, 0, 1));
-		}
-		if (Input.GetKey (KeyCode.E)) {
-			transform.Rotate (new Vector3 (0, 0, -1));
+		//}
+		//if (Input.GetKey (KeyCode.W)) {
+		//	gameObject.GetComponent<Rigidbody> ().AddForce (transform.forward * 15);
+		//}
+		//if (Input.GetKey (KeyCode.RightArrow)) {
+		//	transform.Rotate (new Vector3 (0, 1, 0));
+		//}
+		//if (Input.GetKey (KeyCode.LeftArrow)) {
+		//	transform.Rotate (new Vector3 (0, -1, 0));
+		//}
+		//if (Input.GetKey (KeyCode.UpArrow)) {
+		//	transform.Rotate (new Vector3 (1, 0, 0));
+		//}
+		//if (Input.GetKey (KeyCode.DownArrow)) {
+		//	transform.Rotate (new Vector3 (-1, 0, 0));
+		//}
+		//if (Input.GetKey (KeyCode.Q)) {
+		//	transform.Rotate (new Vector3 (0, 0, 1));
+		//}
+		//if (Input.GetKey (KeyCode.E)) {
+		//	transform.Rotate (new Vector3 (0, 0, -1));
 		}
 	}
 }
